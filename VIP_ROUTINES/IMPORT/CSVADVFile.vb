664015c62ee8211716b529bf4b35bdcfdb6e67b3
LukeS5310 <skywalker5310@gmail.com>
2021-02-15 20:55:40 +0300

    ADV Import Fix

--- VIP_ROUTINES/IMPORT/CSVADVFile.vb
+++ VIP_ROUTINES/IMPORT/CSVADVFile.vb
@@ -9,25 +9,29 @@ Public Class CSVADVFile : Implements ICSVPFR
         Dim SR As New IO.StreamReader(path, System.Text.Encoding.GetEncoding(1251))
         Dim stmp() As String
         While SR.Peek >= 0
-            stmp = SR.ReadLine.Split(";")
-            cmd.CommandText = "INSERT INTO ADV_TABLE (SOURSE,SNILS,FA,IM,OT,RDAT,GENDER,BIRTHPLACE,DEATHDAT,ACTDAT,ACTNUM,ZAGS,SVEDTIME,ID) VALUES(@SOURSE,@SNILS,@FA,@IM,@OT,@RDAT,@GENDER,@BIRTHPLACE,@DEATHDAT,@ACTDAT,@ACTNUM,@ZAGS,@SVEDTIME,@ID)"
-            cmd.Parameters.Add("@SOURSE", DbType.Int16).Value = 1
-            'cmd.Parameters.Add("@RA", DbType.String).Value = stmp(11)
-            cmd.Parameters.Add("@SNILS", DbType.String).Value = stmp(17)
-            cmd.Parameters.Add("@FA", DbType.String).Value = stmp(13)
-            cmd.Parameters.Add("@IM", DbType.String).Value = stmp(15)
-            cmd.Parameters.Add("@OT", DbType.String).Value = stmp(18)
-            cmd.Parameters.Add("@RDAT", DbType.Date).Value = MAIN.validatenull(stmp(28))
-            cmd.Parameters.Add("@GENDER", DbType.String).Value = stmp(27)
-            cmd.Parameters.Add("@BIRTHPLACE", DbType.String).Value = String.Join(" ", stmp(29), stmp(30), stmp(31), stmp(32))
-            cmd.Parameters.Add("@DEATHDAT", DbType.Date).Value = MAIN.validatenull(stmp(34))
-            cmd.Parameters.Add("@ACTDAT", DbType.Date).Value = MAIN.validatenull(stmp(35))
-            cmd.Parameters.Add("@ACTNUM", DbType.String).Value = stmp(36)
-            cmd.Parameters.Add("@SVEDTIME", DbType.DateTime).Value = MAIN.validatenull(stmp(11))
-            cmd.Parameters.Add("@ZAGS", DbType.String).Value = stmp(41)
-            cmd.Parameters.Add("@ID", DbType.String).Value = stmp(14)
-            cmd.ExecuteNonQuery()
-
+            Try
+                stmp = SR.ReadLine.Split(";")
+                cmd.CommandText = "INSERT INTO ADV_TABLE (SOURSE,SNILS,FA,IM,OT,RDAT,GENDER,BIRTHPLACE,DEATHDAT,ACTDAT,ACTNUM,ZAGS,SVEDTIME,ID) VALUES(@SOURSE,@SNILS,@FA,@IM,@OT,@RDAT,@GENDER,@BIRTHPLACE,@DEATHDAT,@ACTDAT,@ACTNUM,@ZAGS,@SVEDTIME,@ID)"
+                cmd.Parameters.Add("@SOURSE", DbType.Int16).Value = 1
+                'cmd.Parameters.Add("@RA", DbType.String).Value = stmp(11)
+                cmd.Parameters.Add("@SNILS", DbType.String).Value = stmp(17)
+                cmd.Parameters.Add("@FA", DbType.String).Value = stmp(13)
+                cmd.Parameters.Add("@IM", DbType.String).Value = stmp(15)
+                cmd.Parameters.Add("@OT", DbType.String).Value = stmp(18)
+                cmd.Parameters.Add("@RDAT", DbType.Date).Value = MAIN.validatenull(stmp(28))
+                cmd.Parameters.Add("@GENDER", DbType.String).Value = stmp(27)
+                cmd.Parameters.Add("@BIRTHPLACE", DbType.String).Value = String.Join(" ", stmp(29), stmp(30), stmp(31), stmp(32))
+                cmd.Parameters.Add("@DEATHDAT", DbType.Date).Value = MAIN.validatenull(stmp(34))
+                cmd.Parameters.Add("@ACTDAT", DbType.Date).Value = MAIN.validatenull(stmp(35))
+                cmd.Parameters.Add("@ACTNUM", DbType.String).Value = stmp(36)
+                cmd.Parameters.Add("@SVEDTIME", DbType.DateTime).Value = MAIN.validatenull(stmp(11))
+                cmd.Parameters.Add("@ZAGS", DbType.String).Value = stmp(41)
+                cmd.Parameters.Add("@ID", DbType.String).Value = stmp(14)
+                cmd.ExecuteNonQuery()
+            Catch ex As Exception
+                MsgBox(String.Format("Пропущена загрузка строки{1}{0}{1}изза ошибки", String.Join(";", stmp), Environment.NewLine))
+                Continue While
+            End Try
         End While
         transact.Commit()
         db.Close()
